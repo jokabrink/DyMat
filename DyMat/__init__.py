b25eee24c4b60e6c6adf142e6e3165de1853f795
Jonas Kock am Brink <jokabrink@posteo.de>
2023-12-07 12:11:45 +0100

    Added function type annotations

--- DyMat/__init__.py
+++ DyMat/__init__.py
@@ -30,7 +30,7 @@ import math
 import os
 import sys
 from math import copysign
-from typing import Union
+from typing import Any, Optional, Union
 
 import numpy
 from scipy.io import loadmat
@@ -73,40 +73,24 @@ class DyMatFile:
         self._blocks = blocks  # block_num: np.ndarray with shape (num_points, num_variables)
         self._absc = abscissa
 
-    def blocks(self):
-        """Returns the numbers of all data blocks.
-
-        :Arguments:
-            - None
-        :Returns:
-            - sequence of integers
-        """
+    def blocks(self) -> list[int]:
+        """Returns the numbers of all data blocks."""
         return self._blocks
 
-    def names(self, block=None):
+    def names(self, block: Optional[int] = None) -> list[str]:
         """Returns the names of all variables. If block is given, only variables of this
         block are listed.
-
-        :Arguments:
-            - optional block: integer
-        :Returns:
-            - sequence of strings
         """
         if block is None:
-            return self._vars.keys()
+            return list(self._vars.keys())
         else:
             return [k for (k, v) in self._vars.items() if v[1] == block]
 
-    def data(self, varName):
-        """Return the values of the variable.
-
-        :Arguments:
-            - varName: string
-        :Returns:
-            - numpy.ndarray with the values"""
+    def data(self, varName: str) -> numpy.ndarray:
+        """Return the values of the variable."""
         tmp, d, c, s = self._vars[varName]
         di = "data_%d" % (d)
-        dd = self.mat[di][c]
+        dd: numpy.ndarray = self.mat[di][c]
         if s < 0:
             dd = dd * -1
         return dd
@@ -114,35 +98,17 @@ class DyMatFile:
     # add a dictionary-like interface
     __getitem__ = data
 
-    def block(self, varName):
-        """Returns the block number of the variable.
-
-        :Arguments:
-            - varName: string
-        :Returns:
-            - integer
-        """
+    def block(self, varName: str) -> int:
+        """Returns the block number of the variable."""
         return self._vars[varName][1]
 
-    def description(self, varName):
-        """Returns the description string of the variable.
-
-        :Arguments:
-            - varName: string
-        :Returns:
-            - string
-        """
+    def description(self, varName: str) -> str:
+        """Returns the description string of the variable."""
         return self._vars[varName][0]
 
-    def sharedData(self, varName):
+    def sharedData(self, varName: str) -> list[tuple[str, float]]:
         """Return variables which share data with this variable, possibly with a different
-        sign.
-
-        :Arguments:
-            - varName: string
-        :Returns:
-            - sequence of tuples, each containing a string (name) and a number (sign)
-        ."""
+        sign."""
         tmp, d, c, s = self._vars[varName]
         return [
             (n, v[3] * s)
@@ -150,14 +116,8 @@ class DyMatFile:
             if n != varName and v[1] == d and v[2] == c
         ]
 
-    def size(self, blockOrName):
-        """Return the number of rows (time steps) of a variable or a block.
-
-        :Arguments:
-            - integer (block) or string (variable name): blockOrName
-        :Returns:
-            - integer
-        """
+    def size(self, blockOrName) -> int:
+        """Return the number of rows (time steps) of a variable or a block."""
         try:
             b = int(blockOrName)
         except:
@@ -165,7 +125,11 @@ class DyMatFile:
         di = "data_%d" % (b)
         return self.mat[di].shape[1]
 
-    def abscissa(self, blockOrName, valuesOnly=False):
+    def abscissa(
+        self,
+        blockOrName,
+        valuesOnly: bool = False,
+    ):
         """Return the values, name and description of the abscissa that belongs to a
         variable or block. If valuesOnly is true, only the values are returned.
 
@@ -190,15 +154,10 @@ class DyMatFile:
                 self._absc[1],
             )
 
-    def sortByBlocks(self, varList):
+    def sortByBlocks(self, varList: list[str]) -> dict[int, list[str]]:
         """Sort a list of variables by the block number, return a dictionary whose keys
         are the block numbers and the values are lists of names. All variables in one
         list will have the same number of values.
-
-        :Arguments:
-            - list of strings: varList
-        :Returns:
-            - dictionary with integer keys and string lists as values
         """
         vl = [(v, self._vars[v][1]) for v in varList]
         vDict = {}
@@ -208,18 +167,13 @@ class DyMatFile:
                 vDict[bl] = l
         return vDict
 
-    def nameTree(self):
+    def nameTree(self) -> dict[str, Any]:
         """Return a tree of all variable names with respect to the path names. Path
         elements are separated by dots. The tree will represent the structure of the
         Modelica models. The tree is returned as a dictionary of dictionaries. The keys
         are the path elements, values are sub-dictionaries or variable names.
-
-        :Arguments:
-            - None
-        :Returns:
-            - dictionary
         """
-        root = {}
+        root: dict[str, Any] = {}
         for v in self._vars.keys():
             branch = root
             elem = v.split(".")
@@ -230,16 +184,14 @@ class DyMatFile:
             branch[elem[-1]] = v
         return root
 
-    def getVarArray(self, varNames, withAbscissa=True):
+    def getVarArray(
+        self,
+        varNames: list[str],
+        withAbscissa: bool = True,
+    ) -> numpy.ndarray:
         """Return the values of all variables in varNames combined as a 2d-array. If
         withAbscissa is True, include abscissa's values first.
         **All variables must share the same block!**
-
-        :Arguments:
-            - sequence of strings: varNames
-            - optional bool: withAbscissa
-        :Returns:
-            - numpy.ndarray
         """
         # FIXME: check blocks!
         v = [numpy.array(self.data(n), ndmin=2) for n in varNames]
@@ -253,14 +205,9 @@ class DyMatFile:
             )
         return numpy.concatenate(v, 0)
 
-    def writeVar(self, varName):
+    def writeVar(self, varName: str) -> None:
         """Write the values of the abscissa and the variabale to stdout. The text format
         is compatible with gnuplot. For more options use DyMat.Export instead.
-
-        :Arguments:
-            - string: varName
-        :Returns:
-            - None
         """
         d = self.data(varName)
         a, aname, tmp = self.abscissa(varName)
